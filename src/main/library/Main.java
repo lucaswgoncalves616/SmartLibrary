ba6f6a26a0e1ed9cb69c972915e9d251a07ae81c
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2025-08-14 09:14:55 -0300

    Feat: Adição de métodos

--- src/main/library/Main.java
+++ src/main/library/Main.java
@@ -1,8 +1,21 @@
 package library;
 import library.model.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        LibraryItems items = new LibraryItems();
+
+        System.out.println("Livros: ");
+        System.out.println(items.getBooksList());
+
+        System.out.println("Revistas: ");
+        System.out.println(items.getMagazinesList());
+
+        System.out.println("DVDs: ");
+        System.out.println(items.getDVDsList());
     }
 }
--- src/main/library/model/LibraryItems.java
+++ src/main/library/model/LibraryItems.java
@@ -3,22 +3,50 @@ package library.model;
 import java.util.ArrayList;
 
 public class LibraryItems{
-    private ArrayList<ArrayList<Object>> books = new ArrayList<>();
-    private ArrayList<ArrayList<Object>> magazines = new ArrayList<>();
-    private ArrayList<ArrayList<Object>> DVDs = new ArrayList<>();
+    private ArrayList<ArrayList<Object>> booksList = new ArrayList<>();
+    private ArrayList<ArrayList<Object>> magazinesList = new ArrayList<>();
+    private ArrayList<ArrayList<Object>> DVDsList = new ArrayList<>();
 
-    public static ArrayList<Object> newBook(int id, String title, int year, boolean loaned, String Author, int pages) {
-        Book newBook = new Book();
+    public ArrayList<ArrayList<Object>> getBooksList() {
+        return booksList;
+    }
 
+    public ArrayList<ArrayList<Object>> getMagazinesList() {
+        return magazinesList;
+    }
+
+    public ArrayList<ArrayList<Object>> getDVDsList() {
+        return DVDsList;
+    }
+
+    public void setBook(int id, String title, int year, boolean loaned, String author, int pages) {
         ArrayList<Object> book = new ArrayList<>();
-
+        book.add(id);
+        book.add(title);
+        book.add(year);
+        book.add(loaned);
+        book.add(author);
+        book.add(pages);
+        booksList.add(book);
     }
 
-    public static ArrayList<Object> newMagazine(int id, String title, int year, boolean loaned, String edition) {
-
+    public void setMagazine(int id, String title, int year, boolean loaned, String edition) {
+        ArrayList<Object> magazine = new ArrayList<>();
+        magazine.add(id);
+        magazine.add(title);
+        magazine.add(year);
+        magazine.add(loaned);
+        magazine.add(edition);
+        magazinesList.add(magazine);
     }
 
-    public static ArrayList<Object> newDVD(int id, String title, int year, boolean loaned, int duration) {
-
+    public void setDVD(int id, String title, int year, boolean loaned, int duration) {
+        ArrayList<Object> DVD = new ArrayList<>();
+        DVD.add(id);
+        DVD.add(title);
+        DVD.add(year);
+        DVD.add(loaned);
+        DVD.add(duration);
+        DVDsList.add(DVD);
     }
 }
